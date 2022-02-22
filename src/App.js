d6774aa00b83584fb46f96a54a3348cbeb95ff42
Akash Nandy <nandyak81@gmail.com>
2022-02-23 01:13:17 +0600

    setup basic things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Client from './components/Client'
+import Footer from './components/Footer'
+import Free from './components/Free'
+import Home from './components/Home'
+import Like from './components/Like'
+import Navbar from './components/Navbar'
+import Release from './components/Release'
+import ScrollToTop from './components/ScrollToTop'
+import SignUp from './components/SignUp'
+import SuperRare from './components/SuperRare'
 
-function App() {
+export default function Hello() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <ScrollToTop />
+      <Navbar />
+      <Home />
+      <Free />
+      <Client />
+      <SuperRare />
+      <Release />
+      <Like />
+      <SignUp />
+      <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
